274d3d54e7063755e8757445050ead91ff6ce4d2
shosh <piglyr@proton.me>
2024-03-27 18:18:36 +0300

    Test

--- Server/main.go
+++ Server/main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
+	message := "mornin'"
 	fmt.Println("jpg")
 	fmt.Println("jpg")
+	fmt.Println("TestText")
+	server, err := net.Listen("tcp", ":4545")
+
+	if err != nil {
+		fmt.Println("Nice job! It doesn't work!")
+		return
+	}
+	defer server.Close()
+
+	for {
+		server, err := server.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		server.Write([]byte(message))
+		server.Close()
+	}
 }
