7fe73d49312aee87e36316dd7657cb73390729a1
Vladislav Kozello <vkozello2005@gmail.com>
2024-03-27 18:10:47 +0300

    Models

--- Server/models.go
+++ Server/models.go
@@ -1,1 +1,37 @@
 package main
+
+type User struct {
+	ID       int
+	Name     string
+	Surname  string
+	Email    string
+	Password string
+	Teacher  string
+	Rating   int
+}
+
+type Good struct {
+	ID          int
+	Name        string
+	Price       float64
+	Picture     string
+	Category    string
+	Description string
+}
+
+type Cart struct {
+	UserID int
+	Goods  []Good
+}
+
+type favotites struct {
+	UserID int
+	Goods  []Good
+}
+
+type Order struct {
+	ID       int
+	SellerID int
+	BuyerID  int
+	Goods    []Good
+}
