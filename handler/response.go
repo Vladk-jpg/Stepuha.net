e8240de997f28a8e9b3880c68b1ec5f129eb17e7
shosh <piglyr@proton.me>
2024-04-28 12:48:47 +0300

    Updated logger to zerolog, for good measure

--- handler/response.go
+++ handler/response.go
@@ -1,9 +1,43 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
+	"github.com/rs/zerolog"
+	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
+	"time"
 )
+
+var buildInfo, _ = debug.ReadBuildInfo()
+
+var loggerParams = zerolog.ConsoleWriter{
+	Out:        os.Stderr,
+	TimeFormat: time.RFC1123,
+	FormatLevel: func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("[%s]", i))
+	},
+	FormatMessage: func(i interface{}) string {
+		return fmt.Sprintf("| %s |", i)
+	},
+	FormatCaller: func(i interface{}) string {
+		return filepath.Base(fmt.Sprintf("%s", i))
+	},
+	PartsExclude: []string{
+		zerolog.TimestampFieldName,
+	},
+}
+
+var logger = zerolog.New(loggerParams).
+	Level(zerolog.TraceLevel).
+	With().
+	Timestamp().
+	Caller().
+	Int("pid", os.Getpid()).
+	Str("go_version", buildInfo.GoVersion).
+	Logger()
 
 type errorResponse struct {
 	Message string `json:"message"`
@@ -14,6 +48,6 @@ type statusResponse struct {
 }
 
 func newErrorResponse(ctx *gin.Context, returnCode int, message string) {
-	log.Printf(message)
+	logger.Print(message)
 	ctx.AbortWithStatusJSON(returnCode, errorResponse{message})
 }
